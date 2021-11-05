4640df0e1bbbb151e767f24db0a2f9edc5eb75b7
Andres Pena <penchjpc@gmail.com>
2021-11-05 15:12:37 -0400

    fall back route and specific note route

---
+++ README.md
@@ -0,0 +1,6 @@
+<!--   PSEUDO CODE -->
+<!--1. Create the route to get past notes
+    2. Create the route to post new notes
+    3. Save notes as JSON object in Local Storage
+    4. 
+ -->
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const path = require('path');
+const eNotes = require('./db/db.json');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.static('public'));
 
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(_dirname,'public/notes.html'))
+);
+// GET route for all existing notes
+app.get('/notes/existing', (req, res)=> res.json(eNotes));
+
+// GET route for specific note
+app.get('/notes/existing/:title', (req, res)=>{
+    const requestedNote = req.params.title.toLocaleLowerCase();
+    // iterate through notes to look for requested title
+    for(let i=0;i<eNotes.length;i++){
+        if(requestedNote === eNotes[i].title.toLocaleLowerCase()){
+            return res.json(eNotes[i]);
+        }
+    }
+    // if requested title not found
+    return res.json('Note not found');
+});
+
+// Fallback route
+app.get('*', (req, res)=>
+     res.send(
+        `Make a GET request to <a href="http://localhost:${PORT}/notes/existing">http://localhost:${PORT}/notes/existing</a>`
+    )
+);
+
+
+// Connections listener
 app.listen(PORT, ()=> 
     console.log(`Server app listening at http://localhost:${PORT}`)
 );
 
-app.get('/notes', (req,res) => 
-    res.sendFile(path.join(_dirname,'public/notes.html'))
-);

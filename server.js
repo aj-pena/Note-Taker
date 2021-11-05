d43f2d22d3e010579663d08c3efe0034cddbc001
Andres Pena <penchjpc@gmail.com>
2021-11-05 14:54:49 -0400

    created existing tasks and notes routes

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const path = require('path');
 
-const app = express ();
+const app = express();
 const PORT = 3001;
 
+app.use(express.static('public'));
+
+app.listen(PORT, ()=> 
+    console.log(`Server app listening at http://localhost:${PORT}`)
+);
+
+app.get('/notes', (req,res) => 
+    res.sendFile(path.join(_dirname,'public/notes.html'))
+);
